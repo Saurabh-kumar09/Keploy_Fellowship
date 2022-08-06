62d40f0e3fa2e63d7c8a0b34285bb662c212ffc3
Saurabh Kumar <89748411+Saurabh-kumar09@users.noreply.github.com>
2022-08-06 15:13:59 +0530

    Update indexw.js

--- session-3_challenge/indexw.js
+++ session-3_challenge/indexw.js
@@ -6,16 +6,19 @@ function callApi() {
     var cname = document.getElementById('cname').value;
     alert(cname);
 
-    const url = `http://api.weatherapi.com/v1/current.json?key={apiId}&q={cname}&aqi=no`;
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiId}&q=${cname}&aqi=no`;
     console.log(url);
 
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
             let output = `
-                <div>
-                <p> ${data.current.temp_c}</p>
-                </div>
+            <div>
+            <div style="color:black; width:12rem; background-color:white; border-radius: 10px;" class="p-3">        
+                <p class="m-0" style="font-width:bold; font-size:2rem;">${data.location.name}</p>
+                <p class="m-0" style="font-width:bold; font-size:4rem;"> ${data.current.temp_c}<sup>°C</sup></p>
+            </div>
+        </div>
                 `;
             console.log('printing data', data);
             document.getElementById('output').innerHTML = output;
